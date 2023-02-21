96f0f186c6273c2a37e54a2300c1a15a08ecda9c
nino-meskhi <meskhi.nino990@gmail.com>
2023-02-22 01:03:20 +0400

    changes in script

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const apiList = document.getElementById("api-list");
+const apiName = document.getElementById("api-name");
+
+function dataFetch() {
+  return fetch(
+    "")
+    .then((response) => response.json())
+    .then((data) => {
+        console.log (data)
+        return data;
+    })
+    .catch ((err) => console.error(err)).
+}
+
