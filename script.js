61d4849769a148f90265e8ab7618242942174d7d
nino-meskhi <meskhi.nino990@gmail.com>
2023-02-22 01:09:07 +0400

    changes in java

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ const apiName = document.getElementById("api-name");
 
 function dataFetch() {
   return fetch(
-    "")
+    "https://bitbucket.org/hpstore/spacex-cargo-planner/raw/204125d74487b1423bbf0453f4dcb53a2161353b/shipments.json"
+  )
     .then((response) => response.json())
     .then((data) => {
-        console.log (data)
-        return data;
+      console.log(data);
+      return data;
     })
-    .catch ((err) => console.error(err)).
+    .catch((err) => console.error(err));
 }
-
