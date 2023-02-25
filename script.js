2911f16ee16b619ad866b15b12b92f5decf41315
nino-meskhi <meskhi.nino990@gmail.com>
2023-02-25 19:42:29 +0400

    cargos calculator

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const apiList = document.getElementById("api-list");
 const apiName = document.getElementById("api-name");
 const apiEmail = document.getElementById("api-email");
 const numOfBoxes = document.getElementById("cargo-input");
-const list = document.getElementById("api-list-ul");
-let compNameslist = [];
+const navigationUl = document.getElementById("api-list-ul");
+const cargosNeeded = document.getElementById("cargos");
+const apiLink =
+  "https://bitbucket.org/hpstore/spacex-cargo-planner/raw/204125d74487b1423bbf0453f4dcb53a2161353b/shipments.json";
 
 function dataFetch() {
-  return fetch(
-    "https://bitbucket.org/hpstore/spacex-cargo-planner/raw/204125d74487b1423bbf0453f4dcb53a2161353b/shipments.json"
-  )
+  return fetch(apiLink)
     .then((response) => response.json())
     .then((data) => {
       return data;
@@ -17,19 +17,32 @@ function dataFetch() {
 }
 
 async function init() {
-  const compInfos = await dataFetch();
-  compNameslist = compInfos.map((item) => {
-    const compNamesItem = document.createElement("li");
-    compNamesItem.id = "li-style";
-    compNamesItem.innerText = item.name;
-    compNamesItem.addEventListener("click", () => {
+  let apiNamesLi = [];
+  const apiFetchedData = await dataFetch();
+  apiNamesLi = apiFetchedData.map((item) => {
+    const apiCompanyNamesList = document.createElement("li");
+    apiCompanyNamesList.id = "li-style";
+    apiCompanyNamesList.innerText = item.name;
+    apiCompanyNamesList.addEventListener("click", () => {
       apiName.textContent = item.name;
-      apiEmail.textContent = item.email;
       numOfBoxes.value = item.boxes;
+      apiEmail.textContent = item.email;
+      count(numOfBoxes.value).then((res) => (cargosNeeded.innerText = res));
     });
-    return compNamesItem;
+    return apiCompanyNamesList;
   });
-  list.append(...compNameslist);
+  navigationUl.append(...apiNamesLi);
+}
+init();
+
+async function count(set) {
+  let key = await set.split(",").map(element => {
+    return Number(element);
+  });
+  let count = 0;
+  key.forEach((el) => {
+    count += el;
+  });
+  return count % 10 === 0 ? count / 10 : Math.ceil(count / 10);
 }
 
-init();
